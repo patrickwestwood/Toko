704a5bb2d0532570854e469dc871b6c1c785adeb
Patryk Zabielski <patrickzabielski@gmail.com>
2014-12-01 21:05:31 +0100

    User cant add link that already been posted

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -10,6 +10,17 @@ Template.postSubmit.events({
             title: $(e.target).find('[name=title]').val()
         };
 
+        Meteor.call('postInsert', post, function(error, result) {
+            // display the error to the user and abort
+            if (error)
+                return alert(error.reason);
+            Router.go('postPage', {_id: result._id});
+        });
+
+        // show this result but route anyway
+        if (result.postExists)
+            alert('This link has already been posted');
+
         post._id = Posts.insert(post);
         Router.go('postPage', post);
     }
